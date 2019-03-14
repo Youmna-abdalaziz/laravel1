77270fc44f15675c81d72f0c82cce4f60cc787a4
Youmna <youmnaabdalaziz@gmail.com>
2019-03-14 19:49:04 +0200

    modify unique validation in update

--- app/Http/Requests/Post/UpdatePostRequest.php
+++ app/Http/Requests/Post/UpdatePostRequest.php
@@ -24,7 +24,7 @@ class UpdatePostRequest extends FormRequest
     public function rules()
     {
         return [
-            'title' => 'required|min:3|unique:posts,title' .$this->id,
+            'title' => 'required|min:3|unique:posts,title,'.$this->post->id,
             'description' => 'required|min:10',
             'user_id' => 'exists:users,id',
         ];
